955bd2e4205317ccb489d87362aa1b85a849c6fb
kimyong <kkskww@gmail.com>
2022-01-23 15:03:20 +0900

    [+]Edit port

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ class movies_genre_movie(Resource):
         return Response(status=200)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
